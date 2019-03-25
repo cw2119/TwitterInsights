21529ff72abccfe44de33a0ff4bdef463ce797d1
cw2119 <47484939+cw2119@users.noreply.github.com>
2019-03-25 21:55:00 +0000

    getting location rather then place

--- search_tweets.py
+++ search_tweets.py
@@ -1,24 +1,77 @@
 from twitter import *
+from datetime import datetime, timedelta
+import time
+import csv
+import sys
+
+def getGender(name):
+
+    trimmedName = (name.partition(' ')[0]).lower()
+
+    file_female = open("female_names.csv", "r")
+    bigString_female = file_female.read()
+    femaleNames = bigString_female.split(',')
+
+    file_male = open("male_names.csv", "r")
+    bigString_male = file_male.read()
+    maleNames = bigString_male.split(',')
+
+
+
+    for femaleName in femaleNames:
+        if trimmedName == femaleName.lower():
+            return "female"
+
+    for maleName in maleNames:
+        if trimmedName == maleName.lower():
+            return "male"
+
+    return "none"
 
 #authentication for the twitter api
 t = Twitter(
     auth=OAuth('1104710748968755201-AqUjzBvEZBmKY4fchydoApwx6JueJS', 'pkTTNrKHradYCIeQayPS9c2iU7gMndWgFjVQ6azNf7JQK', '0AiTG8fZVZHX7c7grH5jJ1BCR', 'KLBcoq9vQy60w1PkCvLlavr4w9qMJrA2gO6UN32lgCnU1EqQO8'))
 
-tweets_bulk =  t.search.tweets(q="#trump", count=100)
+search_hashtag = raw_input("what do you want to search for?")
 
+#Empty list to hold all tweets
+tweets_bulk = []
+
+#Create todays date
+d = datetime.today()
+
+print ("Date today", d)
+
+#Perform the API call 10 times, pushing tweets to the list each time
+for x in range(7):
+    these_tweets = t.search.tweets(q= search_hashtag, result_type='recent', until=d.strftime('%Y-%m-%d'), count=100)
+    tweets_bulk.extend(these_tweets['statuses'])
+    d = d - timedelta(days=1)
+    time.sleep(0.3)
 #print tweets_bulk['statuses']
-
-#making the tweets_bulk information so it contains just the statuses of them
-tweets= tweets_bulk['statuses']
 
 female_count = 0
 male_count = 0
 country_list = []
 
-for tweet in tweets:
+country_occurences = {}
+
+
+print "Number of tweets collected: ", len(tweets_bulk)
+
+for tweet in tweets_bulk:
 
     #Get the user object from the current tweet
     user = tweet['user']
+
+
+    if user['location'] != None:
+        user_country = user['location']
+        if user_country in country_occurences:
+            country_occurences[user_country] = country_occurences[user_country] +1
+        else:
+            country_occurences[user_country]=1
+
 
     #Retrieve their name
     name = user['name']
@@ -33,15 +86,10 @@ for tweet in tweets:
     if gender == 'female':
         female_count = female_count + 1
 
-    if tweet['place']!= None:
-        places = tweet['place']
-        if places['country'] != None:
-            country = places['country'];
-            country_list.append(country)
 
 
-
-print tweets
+print "country occurences", country_occurences
+#print tweets
 
 print "number of males:", male_count
 print "number of females:", female_count
