d6af44ae161c8935c03f4123c9c54e997b60380a
cw2119 <47484939+cw2119@users.noreply.github.com>
2019-03-25 18:59:15 +0000

    twitter authentication

---
+++ search_tweets.py
@@ -0,0 +1,5 @@
+from twitter import *
+
+#authentication for the twitter api
+t = Twitter(
+    auth=OAuth('1104710748968755201-AqUjzBvEZBmKY4fchydoApwx6JueJS', 'pkTTNrKHradYCIeQayPS9c2iU7gMndWgFjVQ6azNf7JQK', '0AiTG8fZVZHX7c7grH5jJ1BCR', 'KLBcoq9vQy60w1PkCvLlavr4w9qMJrA2gO6UN32lgCnU1EqQO8'))
