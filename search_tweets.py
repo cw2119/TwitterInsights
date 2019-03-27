ee168409c50e1085cedfdc78a68595e5d6d24f21
Cici <Cici@138-38-155-43.eduroam.bath.ac.uk>
2019-03-27 17:14:12 +0000

    updating work

--- search_tweets.py
+++ search_tweets.py
@@ -11,145 +11,123 @@ from flask import request
 import webbrowser
 import cgi
 
+#Function makes use of Twitter api to retrieve tweets based on search term passed in
+def get_tweets(search_term):
+    #authentication for the twitter api
+    t = Twitter(
+        auth=OAuth('1104710748968755201-AqUjzBvEZBmKY4fchydoApwx6JueJS', 'pkTTNrKHradYCIeQayPS9c2iU7gMndWgFjVQ6azNf7JQK', '0AiTG8fZVZHX7c7grH5jJ1BCR', 'KLBcoq9vQy60w1PkCvLlavr4w9qMJrA2gO6UN32lgCnU1EqQO8'))
+    #Empty list to hold all tweets
+    tweets_bulk = []
+    #Create todays date
+    d = datetime.today()
+    #Perform the API call 10 times, pushing tweets to the list each time
+    for x in range(7):
+        these_tweets = t.search.tweets(q= search_term, result_type='recent', until=d.strftime('%Y-%m-%d'), count=100)
+        tweets_bulk.extend(these_tweets['statuses'])
+        d = d - timedelta(days=1)
+    number_of_tweets_collected = len(tweets_bulk)
+#returns a list of tweets that have been retrieved by search
+    return tweets_bulk
+
+def get_gender_ratio(tweets):
+    female_count = 0
+    male_count = 0
+    for tweet in tweets:
+        #Get the user object from the current tweet
+        user = tweet['user']
+        #get their name from user
+        name = user['name']
+        #send the name to the getGender method to find out if it is male or female
+        gender = getGender(name)
+        #depending on whether name is male or female, add to count
+        if gender == 'male':
+            male_count = male_count + 1
+        if gender == 'female':
+            female_count = female_count + 1
+#returning ratio of male:female
+    return male_count,":",female_count
 
 
 
-f = open('get_user_input.html','w')
-message = """<html>
-<form action ="" method= "POST">
-  keyword for search:<br>
-  <input type = "text" name ="keyword_for_search"><br>
-  <input type="submit" value="Submit">
-</form>
-</html>"""
 
-f.write(message)
-f.close()
-#get_user_input = 'file:///Macintosh HD/Users/Cici/Documents/CFG/CodeFirstGirls/Twitter_stuff/' + 'get_user_input.html'
-#webbrowser.open_new_tab(get_user_input)
+def getGender(name):
+    #make names all lower case and take away surnames
+    trimmedName = (name.partition(' ')[0]).lower()
+    #importing list of female names from csv file
+    file_female = open("female_names.csv", "r")
+    bigString_female = file_female.read()
+    femaleNames = bigString_female.split(',')
+    #importing list of male names from csv file
+    file_male = open("male_names.csv", "r")
+    bigString_male = file_male.read()
+    maleNames = bigString_male.split(',')
 
-#form = cgi.FieldStorage()
-#keyword_for_search =form.getvalue('keyword_for_search').capitilize()
-#def get_keyword_for_search():
- #  if request.method == 'POST':
-#        keyword_for_search = request.form['key word for search']
-#        return keyword_for_search
-#    else:
-#        return "none"
-#if user_input.html == '__main__':
-#    app.run(host='0.0.0.0')
+    #looping each name through the list of female names and looking for a match
+    for femaleName in femaleNames:
+        if trimmedName == femaleName.lower():
+            return "female"
+    #looping each name through the list of male names and looking for a match
+    for maleName in maleNames:
+        if trimmedName == maleName.lower():
+            return "male"
+    return "none"
+
+def getCountry(tweets):
+    country_list = []
+    top_ten_countries_list = []
+    #a dict for the locations and number of hits per location
+    country_occurences = {}
+    for tweet in tweets:
+        #Get the user object from the current tweet
+        user = tweet['user']
+        #removing tweets with location services switched off
+        if user['location'] != '':
+            user_country = user['location']
+            if user_country in country_occurences:
+                country_occurences[user_country] = country_occurences[user_country] +1
+            else:
+                country_occurences[user_country]=1
+        #sorting the dict of countries
+        sorted_country_occurences = sorted(country_occurences.items(), key=operator.itemgetter(1))
+        #adding the 10 most common countries tweeted from to a list
+        top_ten_countries_list = sorted_country_occurences[-10:]
+    return top_ten_countries_list
+
+def getSentiment(tweets):
+    polarity_list = []
+    for tweet in tweets:
+        #Get the user object from the current tweet
+        user = tweet['user']
+        #getting the text of the tweet
+        tweet_texts = tweet['text']
+        tweet_text = TextBlob(tweet_texts)
+        #getting the polarity for each sentiment and adding this to a list
+        for sentence in tweet_text.sentences:
+            polarity_list.append(sentence.sentiment.polarity)
+    #getting the average polarity
+    polarity_average = np.mean(polarity_list)
+    return polarity_average
+
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('get_user_input.html')
-@app.route('/hello', methods=['POST'])
+
+@app.route('/retrieveTweetData', methods=['POST'])
 def hello():
     keyword_for_search = request.form['get_keyword_for_search']
-    return keyword_for_search
+    #passing key word to get_tweets
+    tweets = get_tweets(keyword_for_search)
+    gender_ratio = get_gender_ratio(tweets)
+    print "gender ratio = ",gender_ratio
+    ten_countries = getCountry(tweets)
+    print "10 countries = ", ten_countries
+    polarity = getSentiment(tweets)
+    print "polarity = ",polarity
+
+    return render_template('results.html', ratio=gender_ratio, tenCountries=ten_countries, sentiment=polarity )
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
 
 hello()
-
-
-def getGender(name):
-
-#
-    trimmedName = (name.partition(' ')[0]).lower()
-
-    file_female = open("female_names.csv", "r")
-    bigString_female = file_female.read()
-    femaleNames = bigString_female.split(',')
-
-    file_male = open("male_names.csv", "r")
-    bigString_male = file_male.read()
-    maleNames = bigString_male.split(',')
-
-
-
-    for femaleName in femaleNames:
-        if trimmedName == femaleName.lower():
-            return "female"
-
-    for maleName in maleNames:
-        if trimmedName == maleName.lower():
-            return "male"
-
-    return "none"
-
-#authentication for the twitter api
-t = Twitter(
-    auth=OAuth('1104710748968755201-AqUjzBvEZBmKY4fchydoApwx6JueJS', 'pkTTNrKHradYCIeQayPS9c2iU7gMndWgFjVQ6azNf7JQK', '0AiTG8fZVZHX7c7grH5jJ1BCR', 'KLBcoq9vQy60w1PkCvLlavr4w9qMJrA2gO6UN32lgCnU1EqQO8'))
-
-#search_hashtag = raw_input("what do you want to search for?")
-
-#Empty list to hold all tweets
-tweets_bulk = []
-
-#Create todays date
-d = datetime.today()
-
-print ("Date today", d)
-
-#Perform the API call 10 times, pushing tweets to the list each time
-for x in range(7):
-    these_tweets = t.search.tweets(q= keyword_for_search, result_type='recent', until=d.strftime('%Y-%m-%d'), count=100)
-    tweets_bulk.extend(these_tweets['statuses'])
-    d = d - timedelta(days=1)
-    #time.sleep(0.3)
-#print tweets_bulk['statuses']
-
-female_count = 0
-male_count = 0
-country_list = []
-top_ten_countries_list = []
-
-country_occurences = {}
-polarity_list = []
-
-
-
-print "Number of tweets collected: ", len(tweets_bulk)
-
-for tweet in tweets_bulk:
-
-    #Get the user object from the current tweet
-    user = tweet['user']
-
-
-    if user['location'] != '':
-        user_country = user['location']
-        if user_country in country_occurences:
-            country_occurences[user_country] = country_occurences[user_country] +1
-        else:
-            country_occurences[user_country]=1
-
-    #Retrieve their name
-    name = user['name']
-
-    gender = getGender(name)
-
-    if gender == 'male':
-        male_count = male_count + 1
-
-    if gender == 'female':
-        female_count = female_count + 1
-
-    tweet_texts = tweet['text']
-    tweet_text = TextBlob(tweet_texts)
-    for sentence in tweet_text.sentences:
-        polarity_list.append(sentence.sentiment.polarity)
-
-
-sorted_country_occurences = sorted(country_occurences.items(), key=operator.itemgetter(1))
-top_ten_countries_list = sorted_country_occurences[-10:]
-#print "Top 10 countries: ", top_ten_countries_list
-
-#print "country occurences", country_occurences
-#print tweets
-
-print "Polarity average: ", np.mean(polarity_list)
-
-print "number of males:", male_count
-print "number of females:", female_count
